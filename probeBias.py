42491ad0b62280106e7a7cdebb4cf1891e128635
liggettla <alexander.liggett@gmail.com>
2016-01-14 16:50:20 -0700

    Now includes both TIII and oncogene probes

--- probeBias.py
+++ probeBias.py
@@ -7,8 +7,10 @@ inputDir = '/media/alex/Extra/Dropbox/Code/fermiData/probeBias'
 outputFile = '/media/alex/Extra/Dropbox/Code/fermiData/probeBias/AverageCoverage.txt'
 output = open(outputFile, 'w')
 
-#These are the oncogene probe sites
-files=('50737', '75775', '75784', '75771', '1152565', '1152587', '1152587', '53428', '53428', '53428', '253982', '253802', '254572', '254572', '2091131', '2091131', '906319', '906318', '486497', '1982668')
+#Oncogene probe sites
+#files=('50737', '75775', '75784', '75771', '1152565', '1152587', '1152587', '53428', '53428', '53428', '253982', '253802', '254572', '254572', '2091131', '2091131', '906319', '906318', '486497', '1982668')
+#TIII probe sites
+files=('115227', '229041', '110541', '112997', '121167', '123547', '124428', '1397', '2126', '2390', '2593', '11486', '92527', '73379', '82455', '85949')
 
 for probe in files:
     inputFile = inputDir + '/' + probe + '.txt'
