70efd776c6d4ec812fc70fe078cc9fdec5f5c4c0
liggettla <alexander.liggett@gmail.com>
2016-01-14 13:50:07 -0700

    First working version

--- probeBias.sh
+++ probeBias.sh
@@ -5,8 +5,12 @@ represented more or not
 '''
 
 INPUT=/home/alex/Desktop/12.22.2015_HiSeq/vcfs/*vcf
-echo 'Probe Loc:' > output.txt
-echo '50737' >> output.txt
-echo '' >> output.txt
-echo 'Coverage:' >> output.txt
-awk -F'[;\t]' '$2 ~ /50737../{sub(/DP=/,"",$15); print $15}' $INPUT >> output.txt
+OUTPUT=/home/alex/Desktop/12.22.2015_HiSeq/output.txt
+
+declare -a arr=("5073770" "7577539" "7578402" "7577119" "115256529" "115258747" "115258744" "534287" "534288" "534289" "25398284" "25380275" "25457242" "25457243" "209113112" "209113113" "90631934" "90631838" "48649700" "198266832" "198266833" "198266834" "198266835")
+for i in "${arr[@]}"
+do
+    echo "$i"
+
+    awk -F'[;\t]' -v var="$i" '$2 ~ var{sub(/DP=/,"",$15); print $15}' $INPUT >> "$i".txt
+done
