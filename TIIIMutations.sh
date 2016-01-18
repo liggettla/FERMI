7bb28a26155dc67bfafb98953360e24d8b792a18
liggettla <alexander.liggett@gmail.com>
2016-01-18 14:23:19 -0700

    Corrected improper matching

--- TIIIMutations.sh
+++ TIIIMutations.sh
@@ -3,4 +3,4 @@
 
 #This pulls the info lines from all matching variants then counts
 #only those that have and AF (column 4) that == 0
-grep '115227*\|229041*\|110541*\|112997*\|121167*\|123547*\|124428*\|1397*\|2126*\|2390*\|2593*\|11486*\|92527*\|73379*\|82455*\|85949*' file.vcf | cut -f 8 | awk -F \; '$4 != 0 {print $4}' | wc -l
+grep '115227*\|229041*\|110541*\|112997*\|121167*\|123547*\|124428*\|1397*\|2126*\|2390*\|2593*\|11486*\|92527*\|73379*\|82455*\|85949*' 16_finalOutput.vcf | cut -f 8 | awk -F \; '{sub(/AF=/,"")} $4 != 1 && $4 != 0.5 {print $4}' | wc -l
