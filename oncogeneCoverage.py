89c963b826f3fd84835b75c1f8354b18d32b12f0
liggettla <alexander.liggett@gmail.com>
2016-01-19 14:33:38 -0700

    Initial Commit

---
+++ oncogeneCoverage.py
@@ -0,0 +1,3 @@
+#The purpose of this script is to take the output from oncogeneCoverage.sh
+#and calculate a total coverage of the probed oncogenes
+
---
+++ oncogeneCoverage.sh
@@ -0,0 +1,5 @@
+#The purpose of this script is to look at the gene coverage of the oncogene
+#sites to look for any coverage differences in samples. This script outputs
+#Dp values to file that can then be processed by oncogeneCovverage.py
+
+
