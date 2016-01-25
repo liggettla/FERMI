c5d4b3e32e551e3cb39979e3eec1a633dfb87c23
liggettla <alexander.liggett@gmail.com>
2016-01-25 13:59:53 -0700

    Initial commit

---
+++ chanceOncMut.py
@@ -0,0 +1,4 @@
+#This purpose of this script is to ask the question of whether or not
+#oncogenic mutations are being seen more often chance would predict based on
+#the frequency of mutations in TIII and non-oncogenic regions as compared
+#with the frequency seen at oncogenic locations
