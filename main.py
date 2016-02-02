9b48f364385eaa968d305c40e11d1641d1355660
liggettla <alexander.liggett@gmail.com>
2016-02-02 13:08:27 -0700

    Fully automated pipeline through variant calling

--- main.py
+++ main.py
@@ -69,6 +69,24 @@ outputCov(twoUmiOut, final_output_file, distance_stringency, coverage_file)
 ####################
 #Align to Reference#
 ####################
+#On cluster first run this command on headnode
+#module load bwa
+
+#@RG for readgroup ID
+#-R proceed with suboptimal aligments, improves paired end mapping at the
+#cost of speed
+#-U 40 increases penalty for unpaired readpair b/c ignoring put right after -M
+#translocations, just looking for single variants
+#-M outputs for picard compatibility
+#-t 13 uses 13 threads (24 per node so uses separate node per sample)
+#-bS - this reads in the binary format sam file and converts to bam file
+#dash tells standard in to read directly from the stream
+
+#bwa mem -R '@RG\tID:'$currentfile.fastq'\tSM:'$currentfile.fastq \
+#    -M $REF $R1 $R2 -t 13 \
+#    | samtools view -bS - \
+#    | samtools sort - $RESDIR/$currentfile.fastq
+
 REF = '/vol3/home/liggettl/refgenomes/hg19.fa'
 bamOut = final_output_file.strip('fastq') + 'bam'
 
@@ -78,13 +96,17 @@ system("samtools index %s" % (bamOut))
 ###############
 #Call Variants#
 ###############
-vcfOut = bamOut.strip(bam) + 'vcf'
+#calls variants only if they are at least 0.00001% of the calls 1/10^6
+#this should thus output every variant found in the alignment
+#by default freebayes uses 0.2 (20%)
+
+vcfOut = bamOut.strip('bam') + 'vcf'
 
 #Cluster
-#system("/vol3/home/liggettl/TruSeqPanel/Scripts/freebayes/freebayes -F 0.0000001 --fasta-reference %s %s > %s" % (REF, bamOut, vcfOut))
+system("/vol3/home/liggettl/TruSeqPanel/Scripts/freebayes/freebayes -F 0.0000001 --fasta-reference %s %s > %s" % (REF, bamOut, vcfOut))
 
 #Lab
-system("/media/alex/Extra/Dropbox/Code/freebayes -F 0.0000001 --fasta-reference %s %s > %s" % (REF, bamOut, vcfOut))
+#system("/media/alex/Extra/Dropbox/Code/freebayes -F 0.0000001 --fasta-reference %s %s > %s" % (REF, bamOut, vcfOut))
 
 '''
 Nothing below this has yet been implemented
