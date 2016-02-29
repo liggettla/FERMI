3306414c5bf3b98225da0893efde4f48d222636f
liggettla <alexander.liggett@gmail.com>
2016-02-29 16:45:49 -0700

    Skipping vcf info lines in a more clear way now

--- varDPFilter.py
+++ varDPFilter.py
@@ -1,12 +1,11 @@
-#The purpose of this script is to take vcf files and filter them based on
-#AF, AO, and DP parameters
-
+#The purpose of this script is to filter the variants output in the final vcf files
+#after running FERMI or the original TrueSeq analysis
 import collections
 from pprint import pprint
 
 inDir = './dataFiles/'
-#inFiles = ['21_finalOutput', '24_finalOutput', '25_finalOutput', '26_finalOutput', '27_finalOutput', '28_finalOutput', '29_finalOutput', '30_finalOutput']
-inFiles = ['28_finalOutput']
+#inFiles = ['10_finalOutput', '13_finalOutput', '18_finalOutput', '20_finalOutput', '6_finalOutput', '9_finalOutput', '11_finalOutput', '14_finalOutput', '16_finalOutput', '19_finalOutput', '2_finalOutput', '7_finalOutput', '12_finalOutput', '15_finalOutput', '17_finalOutput', '1_finalOutput', '5_finalOutput', '8_finalOutput']
+inFiles = ['21_finalOutput', '24_finalOutput', '25_finalOutput', '26_finalOutput', '27_finalOutput', '28_finalOutput', '29_finalOutput', '30_finalOutput']
 
 for file in inFiles:
     input = inDir + file + '.vcf'
@@ -21,30 +20,41 @@ for file in inFiles:
 
     count = 1
     for line in target:
-        if count <= 54:
-            vcfOut.write(line)
-        elif count > 54:
+        if '#' not in line and 'chr' in line: #skip the damn info
             AO = line.split(';')[5]
             DP = line.split(';')[7]
             AF = line.split(';')[3]
+
 
 #these just trim off the AO= or DP= or AF= part of the string
             AONum = int(AO.split(',')[0][3:])
             AFNum = float(AF.split(',')[0][3:])
             DPNum = str(DP.split(',')[0][3:])
 
-
 #filtering parameters
             if int(DPNum) > 500:
                 if AFNum == 0:
-                    if AONum > 0:
-                        vcfOut.write(line + '\n')
+                    if AONum > 5:
+                        vcfOut.write(line)
                         dataOut.write(str(AONum) + '\t' + str(DPNum) + '\t' + str(AFNum) + '\n')
 
-        count+=1
+
+        else: #write the info lines
+            vcfOut.write(line)
+
     target.close()
     vcfOut.close()
     dataOut.close()
 
     #orderedInfoDict = collections.OrderedDict(sorted(infoDict.items()))
 #pprint(orderedInfoDict, output)
+
+
+'''
+        if count <= 54:
+            vcfOut.write(line)
+        elif count > 54:
+            AO = line.split(';')[5]
+            DP = line.split(';')[7]
+            AF = line.split(';')[3]
+'''
