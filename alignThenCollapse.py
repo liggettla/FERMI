bf02b6cb5f1c306838d21b4035963759c2323983
liggettla <alexander.liggett@gmail.com>
2016-03-08 14:45:55 -0700

    Initial commit

---
+++ alignThenCollapse.py
@@ -0,0 +1,7 @@
+#The purpose of the is script is to test the possibility
+#that collapsing before aligning is causing problems (ITS NOT)
+
+inDir = /media/alex/Extra/Dropbox/Code/FERMI/testInput/
+
+R1 = R1.fastq
+R2 = R2.fastq
