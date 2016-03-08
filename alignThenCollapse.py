70b4abb7655b4d783201548317b380709356bca2
liggettla <alexander.liggett@gmail.com>
2016-03-08 15:34:48 -0700

    Added processing for both R1 and R2 for paired end reads

--- alignThenCollapse.py
+++ alignThenCollapse.py
@@ -11,22 +11,31 @@ r2In = open(inDir + r2Name + '.fastq', 'r')
 r1Out = open(outDir + r1Name + '.fastq', 'w')
 r2Out = open(outDir + r2Name + '.fastq', 'w')
 
-count = 1
-plus = '+'
-for line in r1In:
-    if count == 1:
-        header = line.rstrip('\n')
-        count += 1
-    elif count == 2:
-        trimmedSeq = line[6:-6]
-        umiSeq = line[0:6]+line[-6:]
-        umiSeq = umiSeq.rstrip('\n')
-        count += 1
-    elif count == 3:
-        count += 1
-    elif count == 4:
-        trimmedQual = line[6:-6]
-        count += 0
-        r1Out.write(header + ':' + umiSeq + '\n' + trimmedSeq + '\n' + plus + '\n' + trimmedQual + '\n')
+fileList = [r1In, r2In]
+
+for i in fileList:
+    count = 1
+    plus = '+'
+    if i == r1In:
+        inFile = r1In
+        outFile = r1Out
+    elif i == r2In:
+        inFile = r2In
+        outFile = r2Out
+    for line in inFile:
+        if count == 1:
+            header = line.rstrip('\n')
+            count += 1
+        elif count == 2:
+            trimmedSeq = line[6:-6]
+            umiSeq = line[0:6]+line[-6:]
+            umiSeq = umiSeq.rstrip('\n')
+            count += 1
+        elif count == 3:
+            count += 1
+        elif count == 4:
+            trimmedQual = line[6:-6]
+            count += 0
+            outFile.write(header + ':' + umiSeq + '\n' + trimmedSeq + '\n' + plus + '\n' + trimmedQual + '\n')
 
 
