41a55b456b8dd6e96c9f1396b717ea36576394f2
liggettla <alexander.liggett@gmail.com>
2016-03-29 16:25:16 -0600

    Updated comments

--- main.py
+++ main.py
@@ -71,6 +71,7 @@ collapseReadsListDict(seqDict, varThresh, final_output_file, supportingReads, re
 #####################
 outputCov(twoUmiOut, final_output_file, distance_stringency, coverage_file)
 
+'''
 ####################
 #Align to Reference#
 ####################
@@ -92,7 +93,6 @@ outputCov(twoUmiOut, final_output_file, distance_stringency, coverage_file)
 #    | samtools view -bS - \
 #    | samtools sort - $RESDIR/$currentfile.fastq
 
-'''
 #Cluster
 REF = '/vol3/home/liggettl/refgenomes/hg19.fa'
 #Lab
@@ -115,14 +115,12 @@ vcfOut = bamOut.strip('bam') + 'vcf'
 #system("/vol3/home/liggettl/TruSeqPanel/Scripts/freebayes/freebayes -F 0.0000001 --fasta-reference %s %s > %s" % (REF, bamOut, vcfOut))
 #Lab
 system("/media/alex/Extra/Dropbox/Code/freebayes -F 0.0000001 --fasta-reference %s %s > %s" % (REF, bamOut, vcfOut))
-'''
 
 #Using pooled continuous makes frequency based calls without using number of samples as input
 #This might help in adjusting for different copy numbers without knowing the exact number of individual captures
 #in a given reaction
 #freebayes -f %s -F 0.0000001 -C 1 --pooled-continuous %s > %s % (REF, bamOut, vcfOut))
 
-'''
 Nothing below this has yet been implemented
 #######################
 #Output Var Freq Table#
