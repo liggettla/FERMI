f243c3c82bf62973e0a2957bdcd9b66a254015d9
liggettla <alexander.liggett@gmail.com>
2016-04-12 10:40:51 -0600

    fixed rare error when computing coverage and there are no reads

--- goodCollapseDictionary.py
+++ goodCollapseDictionary.py
@@ -233,8 +233,9 @@ def outputCov(distance_stringency):
     outputLines = sum(1 for line in open(final_output_file))
     target.write("Eliminating only exact and close UMI matches:\n")
     target.write("# of Allowed UMI Mismatches: %d\n" %(distance_stringency))
-    target.write("Total # of Original UMIs: %d\n" % (inputLines/4))
-    target.write("# of Unique UMIs: %d\n" % (outputLines/4))
+    if not inputLines/4 == 0:
+        target.write("Total # of Original UMIs: %d\n" % (inputLines/4))
+        target.write("# of Unique UMIs: %d\n" % (outputLines/4))
     #avgCov = float(inputLines)/outputLines
     #target.write("Avg UMI Coverage: %r\n" % (avgCov))
 
