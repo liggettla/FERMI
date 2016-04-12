4b49dae653a6078d517909c0b3b9dbf6bba2923f
liggettla <alexander.liggett@gmail.com>
2016-04-12 10:49:15 -0600

    changed cluster job name to FERMI

--- clusterSubmit.sh
+++ clusterSubmit.sh
@@ -1,6 +1,6 @@
-#BSUB -J UMICon[1]
-#BSUB -e ~/logs/dicCol.%I.%J.err
-#BSUB -o ~/logs/dicCol.%I.%J.out
+#BSUB -J FERMI[1]
+#BSUB -e ~/logs/FERMI%I.%J.err
+#BSUB -o ~/logs/FERMI%I.%J.out
 #BSUB -R "span[hosts=1]"
 #BSUB -n 1
 
