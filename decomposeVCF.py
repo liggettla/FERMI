eebad4bcd22a74e92bc03338c8b12774afcca291
liggetla <alexander.liggett@gmail.com>
2016-05-03 11:35:25 -0600

    included the -s flag to include editing of the info line and allow for proper filtering of the data

--- decomposeVCF.py
+++ decomposeVCF.py
@@ -9,7 +9,7 @@ from os import system
 def decompose(vcfOut):
     decomposeOut = vcfOut.strip('.vcf') + 'Decomposed.vcf'
     blockDecomposedOut = vcfOut.strip('.vcf') + 'BlockDecomposed.vcf'
-    system('vt decompose %s > %s' % (vcfOut, decomposeOut))
+    system('vt decompose -s %s > %s' % (vcfOut, decomposeOut)) # use -s to also edit the info
     system('vt decompose_blocksub %s > %s' % (decomposeOut, blockDecomposedOut))
 
     return blockDecomposedOut
