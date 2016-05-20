4da35989bcbbcc4cf9326a0ee7662aff5eb65281
liggetla <alexander.liggett@gmail.com>
2016-05-20 13:23:06 -0600

    increased memory committment

--- clusterSubmit.sh
+++ clusterSubmit.sh
@@ -1,7 +1,7 @@
 #BSUB -J FERMI[1]
 #BSUB -R "span[hosts=1]"
 #BSUB -n 1
-#BSUB -R "rusage[mem=35]"
+#BSUB -R "rusage[mem=44445]"
 #BSUB -o /vol3/home/liggettl/logs/output.%J -e /vol3/home/liggettl/logs/error.%J job01
 
 python main.py
