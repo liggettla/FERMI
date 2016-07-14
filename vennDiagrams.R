9d2a9a97a67631c8b7a7623ed84128f097c3c50b
liggettla <alexander.liggett@gmail.com>
2016-07-14 15:54:49 -0600

    added plot output functionality

--- vennDiagrams.R
+++ vennDiagrams.R
@@ -2,10 +2,12 @@ library(VennDiagram)
 
 # Sample repeatability between Cord samples 35 and 36 from 3.21.2016 experiment
 # represents 84% and 62% overlap
-grid.newpage()
+#grid.newpage()
+jpeg('cord.jpg')
 draw.pairwise.venn(3063, 4158, 2572, category = c("Cord 1 (Sample 35)", "Cord 2 (Sample 36)"), lty = rep("blank", 
   2), fill = c("blue", "green"), alpha = rep(0.5, 2), cat.pos = c(193, 
   150), cat.dist = rep(0.05, 2), scaled = TRUE)
+dev.off()
 
 # Sample repeatability between 293 samples 33 and 34 from 3.21.2016 experiment
 grid.newpage()
@@ -16,6 +18,8 @@ draw.pairwise.venn(3063, 4158, 2572, category = c("Cord 1 (Sample 35)", "Cord 2 
 # Sample repeatability between B Cells (sample 31) and Myeloid Cells (sample 32) from 3.21.2016 experiment
 # represents a 67% and 73% overlap
 grid.newpage()
+jpeg('BM.jpg')
 draw.pairwise.venn(2496, 2321, 1683, category = c("B Cells (Sample 31)", "Myeloid Cells (Sample 32)"), lty = rep("blank", 
   2), fill = c("blue", "green"), alpha = rep(0.5, 2), cat.pos = c(-20, 
   15), cat.dist = rep(0.05, 2), scaled = TRUE)
+dev.off()
