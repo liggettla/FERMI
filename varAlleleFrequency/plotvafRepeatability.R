5c6f8b79a51e53c2fbcc94476648bfbe0d0e313c
liggetla <alexander.liggett@gmail.com>
2016-07-19 18:13:15 -0600

    changed alpha settings in the plots

--- varAlleleFrequency/plotvafRepeatability.R
+++ varAlleleFrequency/plotvafRepeatability.R
@@ -9,12 +9,12 @@ sample2 <- vafs$Sample2
 lm_fit  = lm(sample1 ~ sample2)
 x = data.frame(vafs, predict(lm_fit, interval = 'prediction'))
 
-p <- ggplot(x, aes(x=sample1,y=sample2)) +
+p <- ggplot(x, aes(x=sample1,y=sample2, alpha=0.5)) +
   xlab('Sample 1') +
   ylab('Sample 2') +
   labs(title = 'VAF Repeatability') +
   geom_point() +
-  geom_smooth(method = 'lm', aes(fill = 'confidence'), alpha = 0.5) +
+  geom_smooth(method = 'lm', aes(fill = 'confidence'), alpha = 0.15) +
   scale_fill_manual('Interval', values = c('green', 'blue'))
 
 jpeg('output.jpg')
