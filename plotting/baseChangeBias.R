45b9c6fab8a7a61e031eabb186cb6b637df0c489
liggetla <alexander.liggett@gmail.com>
2016-10-04 15:41:33 -0600

    Updates

--- plotting/baseChangeBias.R
+++ plotting/baseChangeBias.R
@@ -4,20 +4,20 @@ setwd('/home/alex/Dropbox/Degregori_Lab/Experiments/10.4.2016/A1-R2.fastq')
 oncoGenes <- read.table('oncoGenes.txt', header = TRUE)
 seq=c("A", "C", "T", "G")
 df=data.frame('Ref'=rep(seq, each=4), 'Var'=rep(seq, 4), 'Obs'=oncoGenes$Obs)
-ggplot(data=df) + aes(x=Var, y=Obs, title='C1-R1 OncoGenes') +
+ggplot(data=df) + aes(x=Var, y=Obs, title='A1-R2 OncoGenes') +
   geom_bar(stat='identity', position="dodge", fill="grey", color="black")  +
   facet_grid(~Ref)
 
 oncoSites <- read.table('oncoSites.txt', header = TRUE)
 seq=c("A", "C", "T", "G")
 df=data.frame('Ref'=rep(seq, each=4), 'Var'=rep(seq, 4), 'Obs'=oncoSites$Obs)
-ggplot(data=df) + aes(x=Var, y=Obs, title='C1-R1 OncoSites') + 
+ggplot(data=df) + aes(x=Var, y=Obs, title='A1-R2 OncoSites') + 
   geom_bar(stat='identity', position="dodge", fill="grey", color="black")  +
   facet_grid(~Ref)
 
 TIIIRegions <- read.table('TIIIRegions.txt', header = TRUE)
 seq=c("A", "C", "T", "G")
 df=data.frame('Ref'=rep(seq, each=4), 'Var'=rep(seq, 4), 'Obs'=TIIIRegions$Obs)
-ggplot(data=df) + aes(x=Var, y=Obs, title='C1-R1 TIIIRegions') + 
+ggplot(data=df) + aes(x=Var, y=Obs, title='A1-R2 TIIIRegions') + 
   geom_bar(stat='identity', position="dodge", fill="grey", color="black")  +
   facet_grid(~Ref)
