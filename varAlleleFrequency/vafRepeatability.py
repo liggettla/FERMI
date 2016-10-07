e10327dca0580725601fade7937f62e4037e3ff3
liggettla <alexander.liggett@gmail.com>
2016-10-07 13:23:19 -0600

    Stated an algorithm to include all variants in VAF plots including those that are only found in one of the samples being compared

--- varAlleleFrequency/vafRepeatability.py
+++ varAlleleFrequency/vafRepeatability.py
@@ -14,7 +14,8 @@ parser.add_argument('--indir', '-i', type=str, required=True, help='Input direct
 parser.add_argument('--outdir', '-o', type=str, required=True, help='Output directory for plots: /dir')
 parser.add_argument('--samples', '-s', type=str, nargs='*', required=True, help='Filenames of the two samples to be compared: A1-R1.fastq')
 #parser.add_argument('--rarevars', '-r', action='store_true', help='Only include rare variants in the analysis that are at an AF<0.30')
-parser.add_argument('--rarevars', '-r', type=float, help='This can be set to cutoff the data at a certain allele frequency and only include variants below a particular frequencyc like 0.03 or 0.003.')
+parser.add_argument('--rarevars', '-r', type=float, help='This can be set to cutoff the data at a certain allele frequency and only include variants below a particular frequency like 0.03 or 0.003.')
+parser.add_argument('--commonVars', '-c', action='store_true', help='This will only plot variants that are found in both samples and ignore those variants that are only found in one of the samples.')
 
 args = parser.parse_args()
 
@@ -71,9 +72,31 @@ plotFile = outputDir + '/vafRepeatability.jpg'
 output = open('outputFile', 'w')
 output.write('Sample1\tSample2\tIdentity\n')
 
-for i in df1:
-    if i in df2:
-        output.write('%s\t%s\t%s:%s\n' % (df1[i]['vaf'], df2[i]['vaf'], df1[i]['chr'], i))
+# only output variants observed in both samples
+if args.commonVars:
+    for i in df1:
+        if i in df2:
+            output.write('%s\t%s\t%s:%s\n' % (df1[i]['vaf'], df2[i]['vaf'], df1[i]['chr'], i))
+
+# output all variants including those not observed in both samples
+else:
+    for i in df1:
+        # write overlapping variants
+        if i in df2:
+            output.write('%s\t%s\t%s:%s\n' % (df1[i]['vaf'], df2[i]['vaf'], df1[i]['chr'], i))
+        # write variants found only in df1
+        else:
+            output.write('%s\t%s\t%s:%s\n' % (df1[i]['vaf'], 0, df1[i]['chr'], i))
+
+    # write variants found only in df2
+    '''
+    for i in df2:
+        if i not in df1:
+            output.write('%s\t%s\t%s:%s\n' % (0, df2[i]['vaf'], df1[i]['chr'], i))
+            '''
+
+
+
 
 output.close()
 
@@ -92,6 +115,10 @@ system(command)
 command = 'mv output.jpg %s' % (plotFile)
 system(command)
 
-
+#############
+# Plot Data #
+#############
+command = 'eog vafRepeatability.jpg'
+system(command)
 
 
