ed819e16156cebc892a9068bbb5422123e88cd21
liggettla <alexander.liggett@gmail.com>
2016-10-12 15:29:05 -0600

    Added separate run script

---
+++ varAlleleFrequency/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./multiSampleVAFRep.py -i /media/alex/Extra/Dropbox/Degregori_Lab/Experiments/10.11.2016_SequencingAnalysis/2016-09-20_3_0.6 -o /media/alex/Extra/Dropbox/Code/FERMI/varAlleleFrequency -s C1-R1.fastq E1-R1.fastq -p A1-R1.fastq -r 0.03
