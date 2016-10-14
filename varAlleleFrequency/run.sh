4bb5b45fe92696f36f628a7ab9a3d54101660088
liggettla <alexander.liggett@gmail.com>
2016-10-14 13:22:45 -0600

    Updates

--- varAlleleFrequency/run.sh
+++ varAlleleFrequency/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./multiSampleVAFRep.py -i /media/alex/Extra/Dropbox/Degregori_Lab/Experiments/10.11.2016_SequencingAnalysis/2016-09-20_3_0.6 -o /media/alex/Extra/Dropbox/Code/FERMI/varAlleleFrequency -s E1-R1.fastq F1-R1.fastq G1-R1.fastq H1-R1.fastq A2-R1.fastq B2-R1.fastq -p C1-R1.fastq -r 0.03
+./multiSampleVAFRep.py -i /media/alex/Extra/Dropbox/Degregori_Lab/Experiments/10.11.2016_SequencingAnalysis/2016-09-20_3_0.6 -o /media/alex/Extra/Dropbox/Code/FERMI/varAlleleFrequency -s C1-R1.fastq -p D1-R1.fastq -r 0.003
