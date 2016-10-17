adce7bde4e75dfb93e8aa358248df348ce457ce5
liggettla <alexander.liggett@gmail.com>
2016-10-17 12:44:56 -0600

    Initial commit

---
+++ baseChangeBias/separateBases.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# The purpose of this script is to look at just a particular base change.
+# This was initially motivated by the different base change biases observed
+# when sequencing was performed on different sequencers.
