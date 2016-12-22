48edc01c8b531747d96abfac2c864686bf6e5549
liggettla <alexander.liggett@gmail.com>
2016-12-21 17:21:07 -0700

    Updates

--- sampleOverlap/vennDiagrams.R
+++ sampleOverlap/vennDiagrams.R
@@ -2,18 +2,18 @@ list.of.packages <- c('VennDiagram') # cool little script that checks if ggplot2
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, dep=TRUE)
 library(VennDiagram)
-setwd("~/Desktop")
+setwd("/media/alex/Extra/Dropbox/Degregori_Lab/Experiments/12.21.2016_0.75_120bp_Analysis/sampleOverlap")
 
 jpeg('C1-D1.jpg')
 # venn(sample1, sample2, overlap, ....)
-draw.pairwise.venn(3099, 3040, 2629, category = c("C1 F34", "D1 F34"), lty = rep("blank", 
+draw.pairwise.venn(2288, 2287, 2002, category = c("C1 F34", "D1 F34"), lty = rep("blank", 
   2), fill = c("blue", "green"), alpha = rep(0.5, 2), cat.pos = c(193, 
   150), cat.dist = rep(0.05, 2), scaled = TRUE)
 dev.off()
 
-jpeg('E1-F1.jpg')
+jpeg('C1-E1.jpg')
 # venn(sample1, sample2, overlap, ....)
-draw.pairwise.venn(3134, 2987, 2599, category = c("E1 F41", "F1 F41"), lty = rep("blank", 
+draw.pairwise.venn(2292, 2292, 2009, category = c("E1 F41", "F1 F41"), lty = rep("blank", 
   2), fill = c("blue", "green"), alpha = rep(0.5, 2), cat.pos = c(193, 
   150), cat.dist = rep(0.05, 2), scaled = TRUE)
 dev.off()
