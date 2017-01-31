7c82bd280f68ac3ef122058c4eef8ebb52216146
liggettla <alexander.liggett@gmail.com>
2017-01-31 16:01:51 -0700

    Added anaconda installation of dependencies

---
+++ main/install.sh
@@ -0,0 +1,5 @@
+conda install -c faircloth-lab python-levenshtein
+conda install -c conda install -c anaconda biopython
+conda install -c bioconda freebayes
+conda install -c bioconda vt
+conda install -c bioconda bwa
