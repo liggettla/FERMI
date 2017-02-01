6dea5eee75171e1e78b0aa6a334527bf7dff155b
liggettla <alexander.liggett@gmail.com>
2017-01-31 18:23:55 -0700

    Created method of installation of fermi dependencies into conda environment

---
+++ install/install.sh
@@ -0,0 +1,3 @@
+wget https://repo.continuum.io/archive/Anaconda2-4.3.0-Linux-x86_64.sh
+bash Anaconda2-4.3.0-Linux-x86_64.sh
+conda env create -f fermi.yml
--- main/install.sh
+++
@@ -1,5 +0,0 @@
-conda install -c faircloth-lab python-levenshtein
-conda install -c conda install -c anaconda biopython
-conda install -c bioconda freebayes
-conda install -c bioconda vt
-conda install -c bioconda bwa
