122fadf61fd2eedc29134784d1abd47891d8772b
liggettla <alexander.liggett@gmail.com>
2017-02-01 10:12:11 -0700

    Made installation much easier

--- install/install.sh
+++ install/install.sh
@@ -1,3 +1,7 @@
 wget https://repo.continuum.io/archive/Anaconda2-4.3.0-Linux-x86_64.sh
 bash Anaconda2-4.3.0-Linux-x86_64.sh
 conda env create -f fermi.yml
+cd ../main
+x=pwd
+PATH=$PATH:$x
+echo 'export PATH='$PATH':'$x >> ~/.profile
