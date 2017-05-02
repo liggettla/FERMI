9a1438bf8e75a12b925d3b9db366bed824200b3c
liggettla <alexander.liggett@gmail.com>
2017-05-02 15:11:05 -0600

    plotvafRepeatability.R

--- varAlleleFrequency/run.sh
+++ varAlleleFrequency/run.sh
@@ -3,6 +3,11 @@
 #./multiSampleVAFRep.py -i /home/alex/Dropbox/Degregori_Lab/Experiments/2.13.2017_293_Comparisons/2017-01-09_5_0.55/ -o /home/alex/Dropbox/Code/FERMI/varAlleleFrequency -s e1r1.fastq f1r1.fastq -p d1c1r1.fastq -r 0.003
 #./multiSampleVAFRep.py -i /home/alex/Dropbox/Degregori_Lab/Experiments/2.13.2017_293_Comparisons/2017-01-09_5_0.55 -o /home/alex/Dropbox/Code/FERMI/varAlleleFrequency -s e1r1.fastq -p f1r1.fastq -r 0.003
 
-./multiSampleVAFRep.py -i /media/alex/Extra/Dropbox/Degregori_Lab/Experiments/2.15.2017_Read_Support_Comparisons -o /media/alex/Extra/Dropbox/Code/FERMI/varAlleleFrequency -s 1Capture -p 5Capture -r 0.005
+#./multiSampleVAFRep.py -i /media/alex/Extra/Dropbox/Degregori_Lab/Experiments/2.15.2017_Read_Support_Comparisons -o /media/alex/Extra/Dropbox/Code/FERMI/varAlleleFrequency -s 1Capture -p 5Capture -r 0.005
 
 #./martincorena.py -i /media/alex/Extra/Dropbox/Degregori_Lab/Experiments/10.18.2016_Martincorena -o /media/alex/Extra/Dropbox/Code/FERMI/varAlleleFrequency -s Sample1 Sample4 Sample2 -p Sample3
+
+./multiSampleVAFRep.py -i /media/alex/Extra/Dropbox/Code/Sequencing/5.2.2017_InitialAnalysis_4.1.17/2017-05-01_5_0.75 -o /media/alex/Extra/Dropbox/Code/FERMI/varAlleleFrequency -s 3r1.fastq 4r1.fastq 5r1.fastq 6r1.fastq 7r1.fastq 8r1.fastq 9r1.fastq 10r1.fastq 11r1.fastq 12r1.fastq 13r1.fastq 14r1.fastq 15r1.fastq 16r1.fastq 17r1.fastq 18r1.fastq 19r1.fastq 20r1.fastq 21r1.fastq 22r1.fastq -p 4r1.fastq -r 0.02 -c
+
+### 4.1.2017 sample list
+# 1r1.fastq 2r1.fastq 3r1.fastq 4r1.fastq 5r1.fastq 6r1.fastq 7r1.fastq 8r1.fastq 9r1.fastq 10r1.fastq 11r1.fastq 12r1.fastq 13r1.fastq 14r1.fastq 15r1.fastq 16r1.fastq 17r1.fastq 18r1.fastq 19r1.fastq 20r1.fastq 21r1.fastq 22r1.fastq 23r1.fastq 24r1.fastq
