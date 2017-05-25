f3390daa2867f6d272041f57dae781b0916d98ac
liggetla <alexander.liggett@gmail.com>
2017-05-24 20:07:40 -0600

    Initial commit only missing plotting functionality

---
+++ selectionTesting/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./oncogenicChanges -i /home/alex/Dropbox/Code/Sequencing/5.2.2017_InitialAnalysis_4.1.17/2017-05-01_5_0.75 -o /home/alex/Dropbox/Code/FERMI/selectionTesting -k samplekey.txt -s 10r1.fastq 12r1.fastq 14r1.fastq 16r1.fastq 18r1.fastq 1r1.fastq 21r1.fastq 23r1.fastq 2r1.fastq 4r1.fastq 6r1.fastq 8r1.fastq 11r1.fastq 13r1.fastq 15r1.fastq 17r1.fastq 19r1.fastq 20r1.fastq 22r1.fastq 24r1.fastq 3r1.fastq 5r1.fastq 7r1.fastq 9r1.fastq
+#./oncogenicChanges -i /home/alex/Dropbox/Code/Sequencing/5.2.2017_InitialAnalysis_4.1.17/2017-05-01_5_0.75 -o /home/alex/Dropbox/Code/FERMI/selectionTesting -k samplekey.txt -s 10r1.fastq
