bdbd325ef70fcb9ea6fe1e104e78b849c19a6443
liggettla <alexander.liggett@gmail.com>
2017-11-28 12:16:32 -0700

    Flanking sequence now performs much faster by using a reference genome instead of the genome browser

--- vcfToSeq/getSequence.py
+++ vcfToSeq/getSequence.py
@@ -3,14 +3,14 @@
 # this takes a line from a vcf file and outputs sequence of specified length
 # flanking mutated locus
 def getSequence(vcfLine, flankLength):
-    from parseline import parseLine
+    from parseline import VCFObj
     from subprocess import check_output, STDOUT
 
-    location, AFNum, WT, var, loc, chrom = parseLine(vcfLine)
+    vcfObj = VCFObj(vcfLine)
 
-    low = int(loc) - flankLength
-    high = int(loc) + flankLength
-    temp = check_output('wget -qO- http://genome.ucsc.edu/cgi-bin/das/hg19/dna?segment=%s:%s,%s' % (chrom,low,high), stderr=STDOUT, shell=True)
+    low = int(vcfObj.location) - flankLength
+    high = int(vcfObj.location) + flankLength
+    temp = check_output('wget -qO- http://genome.ucsc.edu/cgi-bin/das/hg19/dna?segment=%s:%s,%s' % (vcfObj.chrom,low,high), stderr=STDOUT, shell=True)
 
     finalSeq = ''
     for line in temp.split('\n'):
@@ -18,3 +18,28 @@ def getSequence(vcfLine, flankLength):
             finalSeq += line
 
     return finalSeq
+
+# this provides similar functionality to getSequence, but instead of using the
+# USCS genome browser for sequence, it uses a reference genome
+def getRefSequence(vcfLine, flankLength, ref):
+    from parseline import VCFObj
+    from subprocess import check_output, STDOUT
+    from string import upper
+
+    vcfObj = VCFObj(vcfLine)
+
+    low = int(vcfObj.location) - flankLength
+    high = int(vcfObj.location) + flankLength
+    temp = check_output('samtools faidx %s %s:%s-%s' % (ref, vcfObj.chrom, low, high), stderr=STDOUT, shell=True)
+
+    finalSeq = ''
+    for line in temp.split('\n'):
+        if '>' not in line:
+            finalSeq += line
+
+    finalSeq = finalSeq.upper()
+    return finalSeq
+
+
+
+
